0a5306574c9e4ec7a38fa826d0929dd07b623a32
Maxim Aleynikov <max.aleyinikov@gmail.com>
2017-11-19 23:27:44 +0200

    Added base logic with work API.

--- classes/Swift/SmartSenderTransport.php
+++ classes/Swift/SmartSenderTransport.php
@@ -2,29 +2,103 @@
 
 class Swift_SmartSenderTransport extends Swift_Transport_SmartSenderTransport
 {
-    public function isStarted()
+    private $key;
+    private $secret;
+    private $endpoint;
+
+    private $response;
+
+    public function __construct($key, $secret, $endpoint = 'https://api.sndmart.com/send')
     {
-        // TODO: Implement isStarted() method.
+        $this->key      = $key;
+        $this->secret   = $secret;
+        $this->endpoint = $endpoint;
     }
 
-    public function start()
+    public static function newInstance($key, $secret)
     {
-        // TODO: Implement start() method.
+        return new self($key, $secret);
     }
 
-    public function stop()
+    public function setKey($val)
     {
-        // TODO: Implement stop() method.
+        $this->key = $val;
+    }
+
+    public function getKey()
+    {
+        return $this->key;
+    }
+
+    public function setSecret($val)
+    {
+        $this->secret = $val;
+    }
+
+    public function getSecret()
+    {
+        return $this->secret;
+    }
+
+    public function setEndpoint($val)
+    {
+        $this->endpoint = $val;
+    }
+
+    public function getEndpoint()
+    {
+        return $this->endpoint;
+    }
+
+    public function getResponse()
+    {
+        return $this->response;
     }
 
     public function send(Swift_Mime_Message $message, &$failedRecipients = null)
     {
-        echo 'send message via smartsender transport...';
+        $data = [
+            'key'     => $this->key,
+            'secret'  => $this->secret,
+            'message' => [
+                'from_email' => $message->getFrom(),
+                'reply_to'   => [
+                    [
+                        'email' => $message->getReplyTo(),
+                    ]
+                ],
+                'subject'    => $message->getSubject(),
+                'to'         => [
+                    [
+                        'email' => $message->getTo(),
+                    ]
+                ],
+                'text'       => $message->getBody(),
+                'html'       => $message->getBody(),
+            ],
+        ];
+
+        $this->response = $this->_doRequest($data);
     }
 
-    public function registerPlugin(Swift_Events_EventListener $plugin)
+    protected function _doRequest(array $data)
     {
-        // TODO: Implement registerPlugin() method.
-    }
+        $data = json_encode($data);
+        $ch = curl_init($this->endpoint);
 
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt(
+            $ch, CURLOPT_HTTPHEADER,
+            [
+                'Content-Type: application/json',
+                'Content-Length: ' . strlen($data)
+            ]
+        );
+
+        $result = curl_exec($ch);
+        curl_close($ch);
+        return $result;
+    }
 }
--- classes/Swift/Transport/SmartSenderTransport.php
+++ classes/Swift/Transport/SmartSenderTransport.php
@@ -2,5 +2,23 @@
 
 abstract class Swift_Transport_SmartSenderTransport implements Swift_Transport
 {
+    public function isStarted()
+    {
+        // TODO: Implement isStarted() method.
+    }
 
+    public function start()
+    {
+        // TODO: Implement start() method.
+    }
+
+    public function stop()
+    {
+        // TODO: Implement stop() method.
+    }
+
+    public function registerPlugin(Swift_Events_EventListener $plugin)
+    {
+        // TODO: Implement registerPlugin() method.
+    }
 }
